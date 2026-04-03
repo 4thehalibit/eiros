fc92a088b2713905d81a2c10f4b155a1c5ab9fb6
Lyle Cleveland <lyle.cleveland@proton.me>
2026-04-03 09:26:08 -0500

    fix distrobox enable logic

--- system/virtualization/distrobox.nix
+++ system/virtualization/distrobox.nix
@@ -13,7 +13,7 @@ in
     description = "Enable distrobox";
     type = lib.types.bool;
   };
-  config = lib.mkIf (eiros_virtualization.enable && eiros_virtualization.podman.enable) {
+  config = lib.mkIf (eiros_virtualization.enable && eiros_virtualization.distrobox.enable) {
     environment.systemPackages = [ pkgs.distrobox ];
   };
 }
