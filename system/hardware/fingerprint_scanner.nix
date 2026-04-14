5448a196c976a4804bcfc3a1eee2c13fd40c91af
Lyle Cleveland <lyle.cleveland@proton.me>
2026-04-14 09:42:57 -0500

    revert

--- system/hardware/fingerprint_scanner.nix
+++ system/hardware/fingerprint_scanner.nix
@@ -30,36 +30,12 @@ in
   };
   config = lib.mkIf eiros_fingerprint.enable {
     services.fprintd.enable = true;
-    security.pam.services =
-      builtins.listToAttrs (
-        map (service_name: {
-          name = service_name;
-          value.fprintAuth = true;
-        }) eiros_fingerprint.pam_services
-      )
-      // {
-        greetd.text = ''
-          # Account management.
-          account required ${pkgs.linux-pam}/lib/security/pam_unix.so
 
-          # Authentication management.
-          auth optional ${pkgs.linux-pam}/lib/security/pam_unix.so likeauth nullok
-          auth optional ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
-          auth sufficient ${pkgs.linux-pam}/lib/security/pam_unix.so likeauth nullok try_first_pass
-          auth required ${pkgs.linux-pam}/lib/security/pam_deny.so
-
-          # Password management.
-          password sufficient ${pkgs.linux-pam}/lib/security/pam_unix.so nullok yescrypt
-          password optional ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so use_authtok
-
-          # Session management.
-          session required ${pkgs.linux-pam}/lib/security/pam_env.so conffile=/etc/pam/environment readenv=0
-          session required ${pkgs.linux-pam}/lib/security/pam_unix.so
-          session required ${pkgs.linux-pam}/lib/security/pam_loginuid.so
-          session optional ${pkgs.systemd}/lib/security/pam_systemd.so
-          session required ${pkgs.linux-pam}/lib/security/pam_limits.so conf=/nix/store/d7hsf3gq9mjfd8ncs9bcx6lbhy3r2ddc-limits.conf
-          session optional ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-        '';
-      };
+    security.pam.services = builtins.listToAttrs (
+      map (service_name: {
+        name = service_name;
+        value.fprintAuth = true;
+      }) eiros_fingerprint.pam_services
+    );
   };
 }
